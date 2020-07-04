411cb7d84961e2e6ab8b7904d7ef08d335b47472
Moldir Istayeva <moldiristayeva@Moldirs-MBP-2.hsd1.il.comcast.net>
2020-07-03 21:51:28 -0500

    git log ghanges

--- src/packg1/Test3.java
+++ src/packg1/Test3.java
@@ -3,7 +3,7 @@ package packg1;
 public class Test3 {
     public static void main(String[] args) {
         System.out.println("Third file");
-        System.out.println("askar we did itwq"wq);
+        System.out.println("git log changes");
     }
 }
 
